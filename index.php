7e03f16f52751f50860b07f52ffa40a602c8f8e9
Davidzyt <yuntian.zou@hotmail.com>
2021-05-17 15:19:48 +0200

    creating repo

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>POKEDEX-PHP</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<h1>POKEDEX-PHP</h1>
+
+<form action="">
+    <input type="text" name="search">
+    <button type="submit">Search POKEMON</button>
+</form>
+
+<section id="pokemon-stats">
+    <?php
+    echo "Pokemon"
+    ?>
+</section>
+
+</body>
+</html>
