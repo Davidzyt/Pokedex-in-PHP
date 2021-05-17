8edf35692f648a44dd909abd51dd7b208c560df9
Davidzyt <yuntian.zou@hotmail.com>
2021-05-17 21:53:28 +0200

    edit REAMME file;adding some codes in HTML

--- index.php
+++ index.php
@@ -2,25 +2,29 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>POKEDEX-PHP</title>
-    <link rel="stylesheet" href="./style.css">
+    <title>POKEMON-PHP Version</title>
 </head>
 <body>
-<h1>POKEDEX-PHP</h1>
+<h1>POKEMON - PHP Version</h1>
 
-<form action="">
-    <input type="text" name="search">
-    <button type="submit">Search POKEMON</button>
-</form>
-
-<section id="pokemon-stats">
-    <?php
-    echo "Pokemon"
-    ?>
+<section id="search-box" class="section">
+    <form action="">
+        <label>
+            Search for a pokemon!
+            <br>
+            <input type="text" name="pokemon">
+        </label>
+        <br>
+        <button type="submit">Search POKEMON</button>
+    </form>
 </section>
 
+<section id="pokemon-stats" class="section">
+    <?php
+    $pokemon_name = '';
+    $pokemon_id = '';
+    $pokemon_image = '';
+    $pokemon_evolutions = '';
+?>
 </body>
 </html>
